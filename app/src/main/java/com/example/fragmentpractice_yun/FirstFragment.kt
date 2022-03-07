4eeaac816f2b4854e716975d301a6a4ae7e142f3
Roy <J.jyun0525@gmail.com>
2022-03-07 14:34:03 +0900

    첫번째 프래그먼트 동작처리 + 메인화면xml

--- app/src/main/java/com/example/fragmentpractice_yun/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentpractice_yun/FirstFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -14,5 +15,15 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first,container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        changeTextBtn.setOnClickListener {
+
+//            임시로 텍스트 문구를 "안녕하세요"라고 변경
+            contentTxt.text = "안녕하세요."
+        }
     }
 }
